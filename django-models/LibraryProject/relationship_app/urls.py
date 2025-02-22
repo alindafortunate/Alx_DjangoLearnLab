76edb8c2e72fb2d09395d473ba864922af738f03
alindafortunate <alindafortunate5@gmail.com>
2025-02-22 23:11:14 +0300

    Changed to a login function

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,17 +1,23 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView, LogoutView
 
 from .views import list_books
 from .views import LibraryDetailView
-from .views import SignUpView
-from .views import LoginView
-from .views import LogoutView
+from . import views
 
 
 urlpatterns = [
     path("", list_books, name="list_books"),
     path("library/<pk>/", LibraryDetailView.as_view(), name="library_detail"),
-    path("relationship_app/login", LoginView, name="login"),
-    path("relationship_app/logout/", LogoutView, name="logout"),
-    path("relationship_app/register/", SignUpView.as_view(), name="register"),
+    path(
+        "relationship_app/login",
+        LoginView.as_view(template_name="registration/login.html"),
+        name="login",
+    ),
+    path(
+        "relationship_app/logout/",
+        LogoutView.as_view(template_name="registration/login.html"),
+        name="logout",
+    ),
+    path("relationship_app/register/", views.register.as_view(), name="register"),
 ]
