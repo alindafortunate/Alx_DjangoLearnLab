f91f0dc9477a68b488468fa2173da9c86676c75f
alindafortunate <alindafortunate5@gmail.com>
2025-02-22 12:42:09 +0300

    Initial commit

---
+++ django-models/LibraryProject/README.md
@@ -0,0 +1,2 @@
+My Django project.
+I will continue programming
---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,12 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+# Query all books by a specific author.
+Book.objects.filter(author__id=1)
+
+# List all books in a library.
+
+library1 = Library.objects.create(name="Programming")
+library1.books.all()
+
+# Retrieve the librarian for a library.
+Librarian.objects.get(library__id=1)
