28c6c5a69a62a26de4085b0ccec37f0ee82e32ea
alindafortunate <alindafortunate5@gmail.com>
2025-02-24 01:29:20 +0300

    Initial commit for task4

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -84,14 +84,17 @@ def librarian_view(request):
 def member_view(request):
     return render(request, "relationship_app/member_view.html")
 
+
 @permission_required(relationship_app.can_add_book)
 def add_book(request):
     return HttpResponse("Add a book.")
 
 
+@permission_required(relationship_app.can_change_book)
 def edit_book(request):
     return HttpResponse("edit a book")
 
 
+@permission_required(relationship_app.can_delete_book)
 def delete_book(request):
     return HttpResponse("Delete a book")
