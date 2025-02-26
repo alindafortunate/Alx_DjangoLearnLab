b71747f292f172b2cae5a748a398c90efd63923b
alindafortunate <alindafortunate5@gmail.com>
2025-02-26 12:55:01 +0300

    Transfered the models to bookshelf

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,7 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
+
 
 
 # Create your models here.
+class CustomUserManager(BaseUserManager):
+    def creat_user(self, email, password=None):
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.creat_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True, max_length=256)
+    date_of_birth = models.DateField(null=True)
+    profile_photo = models.ImageField(upload_to=None)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,37 +1,9 @@
-from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-
 # Create your models here.
-class CustomUserManager(BaseUserManager):
-    def creat_user(self, email, password=None):
-        user = self.model(email=self.normalize_email(email))
-        user.set_password(password)
-        user.save(using=self._db)
-
-        return user
-
-    def create_superuser(self, email, password=None):
-        user = self.creat_user(email, password)
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-
-        return user
-
-
-class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True, max_length=256)
-    date_of_birth = models.DateField(null=True)
-    profile_photo = models.ImageField(upload_to=None)
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
-
-    objects = CustomUserManager()
 
 
 class Author(models.Model):
@@ -82,7 +54,7 @@ class UserProfile(models.Model):
     )
 
     user = models.OneToOneField(
-        CustomUser, on_delete=models.CASCADE, related_name="userprofile"
+        User, on_delete=models.CASCADE, related_name="userprofile"
     )
     role = models.CharField(max_length=255, choices=USER_ROLES)
 
@@ -93,10 +65,10 @@ class UserProfile(models.Model):
         if created:
             UserProfile.objects.create(user=instance)
 
-    post_save.connect(create_profile, sender=CustomUser)
+    post_save.connect(create_profile, sender=User)
 
     def update_profile(sender, instance, created, **kwargs):
         if created is False:
             instance.userprofile.save()
 
-    post_save.connect(update_profile, sender=CustomUser)
+    post_save.connect(update_profile, sender=User)
