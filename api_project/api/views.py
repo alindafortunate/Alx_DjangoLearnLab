7e8e546cc4c6b7d5f4b7a901ab24371c3a51be5e
alindafortunate <alindafortunate5@gmail.com>
2025-03-07 10:28:46 +0300

    Added Permissions to settings.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -9,6 +9,8 @@ from .serializers import BookSerializer
 
 # Create your views here.
 class BookList(generics.ListAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsAdminUser]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
