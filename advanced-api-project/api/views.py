12405acbb5517e9be8c547b93b3c3243b637faf3
alindafortunate <alindafortunate5@gmail.com>
2025-03-14 22:59:54 +0300

    Added filtering, searching, and ordering features

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from django_filters import rest_framework
 
-# from rest_framework import filters
+from rest_framework import filters
 from .models import Book
 from .serializers import BookSerializer
 
@@ -17,7 +17,7 @@ class BookListView(generics.ListAPIView):
     serializer_class = BookSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fileds = ["title", "author", "publication_year"]
-    filter_backends = [rest_framework.filters.SearchFilter]
+    filter_backends = [filters.SearchFilter]
     search_fields = ["title", "author"]
     filter_backends = [rest_framework.filters.OrderingFilter]
     ordering_fields = ["title", "publication_year"]
