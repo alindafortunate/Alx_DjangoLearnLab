fd640ef22740b0b960ee9da470aac2ed52bf3789
alindafortunate <alindafortunate5@gmail.com>
2025-03-19 14:28:39 +0300

    Imported the user model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
 CustomUser = get_user_model
 
@@ -14,6 +15,4 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(
-        CustomUser, on_delete=models.CASCADE, related_name="posts"
-    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
