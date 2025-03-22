075162744caa77bdf1cdedb742a64dd658266b46
alindafortunate <alindafortunate5@gmail.com>
2025-03-22 12:43:27 +0300

    Added authentication views

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -9,9 +9,6 @@ class CustomUser(AbstractUser):
     pass
 
 
-# CustomUser = User
-
-
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 # Create your views here.
+def register(request):
+    pass
+
+
+def profile(request):
+    pass
