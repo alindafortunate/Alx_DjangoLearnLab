41085df0c8b5e1cc6d760b0313039397352fe870
alindafortunate <alindafortunate5@gmail.com>
2025-03-22 14:18:01 +0300

    Sampled on the profile and register view

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 
 # Create your views here.
 def register(request):
-    pass
+    form = UserCreationForm
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+    return render(request, "blog/register.html")
 
 
 def profile(request):
-    pass
+    form = UserChangeForm
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+
+    return render(request, "blog/profile.html")
