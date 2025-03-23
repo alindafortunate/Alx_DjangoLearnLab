b0d32269de4b0377e97038e6b4f7bfa1f362e766
alindafortunate <alindafortunate5@gmail.com>
2025-03-23 16:05:43 +0300

    Tried on the update_profile view

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
 from django.forms import ModelForm
 from .models import Post
 
@@ -6,3 +9,21 @@ class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content", "author"]
+
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True, label="Enter Email")
+
+    class Meta(UserCreationForm.Meta):
+
+        fields = ["username", "email", "password1", "password2"]
+
+    def save(self, commit=True):
+
+        user = User.objects.create_user(
+            self.cleaned_data["username"],
+            self.cleaned_data["email"],
+            self.cleaned_data["password1"],
+        )
+
+        return user
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView,
     DetailView,
@@ -11,6 +12,7 @@ from django.views.generic import (
 
 # from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
+from .forms import CustomUserCreationForm
 
 
 # Create your views here.
@@ -19,24 +21,33 @@ def index(request):
 
 
 def register(request):
-    form = UserCreationForm()
+
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("/login/")
+    else:
+        form = CustomUserCreationForm()
     context = {
         "form": form,
     }
     return render(request, "blog/register.html", context)
 
 
+class ProfileManagement(UpdateView):
+    pass
+
+
 def profile(request):
-    form = UserChangeForm()
     if request.method == "POST":
-        form = UserChangeForm(request.POST)
+        user = User.objects.get(user=request.user)
+        form = CustomUserCreationForm(request.POST, instance=user)
         if form.is_valid():
             form.save()
+            return redirect("/login/")
+    else:
+        form = CustomUserCreationForm()
     context = {"form": form}
 
     return render(request, "blog/profile.html", context)
