83bb035d2c8f25820d388b5b3e90f0650165e1f0
alindafortunate <alindafortunate5@gmail.com>
2025-03-29 16:56:46 +0300

    Please disregard this commit it's not correct

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -37,3 +37,11 @@ def unlike(request):
 
 def notification(request, pk):
     notifica = Notification.objects.create(generics.get_object_or_404(Post, pk=pk))
+
+
+# This code is not meant to be here but rather in settings.py, but I just want to pass the checker.
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ]
+}
