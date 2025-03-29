72543952fcf44eabf970ced221ae268c269604a0
alindafortunate <alindafortunate5@gmail.com>
2025-03-29 14:34:51 +0300

    Added the Post and Comment models

---
+++ social_media_api/posts/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_created=True)
+
+
+class Comment(models.Model):
+    comments = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="author_comments"
+    )
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_created=True)
